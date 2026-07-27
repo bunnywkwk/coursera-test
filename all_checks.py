8f7d9775c6acf5d7342fb09141bcab833aeae6e9
bunnywkwk <aeronsolano06@gmail.com>
2026-07-27 13:33:36 +0800

    Add some function

--- all_checks.py
+++ all_checks.py
@@ -2,13 +2,28 @@
 
 import os
 import sys
+import shutil
 
 def check_reboot():
 	return os.path.exists("/run/reboot-required")
 
+def check_disk_full(disk, min_gb, min_percent):
+    du = shutil.disk_usage(disk)
+
+    percent_free = 100 du.free / du.total
+
+    gigabytes_free = du.free / 2**30
+    if percent_free < min_percent or gigabytes_free < min_gb
+        return True
+    return False
+
+
 def main():
 	if check_reboot():
 		print("Pending Reboot.")
+		sys.exit(1)
+	if check_disk_full(disk="/", min_gb=2, min_percent=10)
+		print("Disk full.")
 		sys.exit(1)
 
 	print("Everything OK.")
