eaa546d4a4134f1fad069755f6217b2862efaabc
bunnywkwk <aeronsolano06@gmail.com>
2026-07-27 13:55:43 +0800

    change var name

--- all_checks.py
+++ all_checks.py
@@ -10,12 +10,11 @@ def check_reboot():
 def check_disk_full(disk, min_gb, min_percent):
     du = shutil.disk_usage(disk)
 
-    percent_free = 100 du.free / du.total
+    percent_free = 100 * du.free / du.total
 
     gigabytes_free = du.free / 2**30
 
-    if percent_free < min_percent or gigabytes_free < min_gigabytes
->>>>>>> main
+    if percent_free < min_percent or gigabytes_free < min_gb
         return True
     return False
 
